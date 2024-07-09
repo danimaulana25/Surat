046caf5a006fa1b3eb7bcb13939d8dce2d692253
danimaulana25 <ahmaddanimaulana80@gmail.com>
2024-07-09 15:21:44 +0700

    add template

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,43 @@
+<div class="sidebar">
+    <!-- SidebarSearch Form -->
+    <div class="form-inline mt-2">
+        <div class="input-group" data-widget="sidebar-search">
+            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+            <div class="input-group-append">
+                <button class="btn btn-sidebar">
+                    <i class="fas fa-search fa-fw"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item">
+                <a href="{{ url('/') }}" class="nav-link {{ $activeMenu == 'dashboard' ? 'active' : '' }} ">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            <li class="nav-header">Menu</li>
+            <li class="nav-item">
+                <a href="{{ url('/kategori') }}" class="nav-link {{ $activeMenu == 'kategori' ? 'active' : '' }} ">
+                    <i class="nav-icon far fa-bookmark"></i>
+                    <p>Arsip</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ url('/stok') }}" class="nav-link {{ $activeMenu == 'stok' ? 'active' : '' }} ">
+                    <i class="nav-icon far fa-folder-open"></i>
+                    <p>Kategori</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ url('/barang') }}" class="nav-link {{ $activeMenu == 'barang' ? 'active' : '' }} ">
+                    <i class="nav-icon fas fa-exclamation-circle"></i>
+                    <p>About</p>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dash', function () {
+    $breadcrumb = (object) [
+        'title' => 'Selamat Datang',
+        'list'  => ['Home', 'Welcome']
+    ];
+
+    $activeMenu = 'dashboard';
+
+    return view('dashboard', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+});
