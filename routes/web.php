4b23c12a4e8a64803b1b45173bbfd78bcf7fb26d
danimaulana25 <ahmaddanimaulana80@gmail.com>
2024-07-11 07:07:43 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArsipController;
+use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dash', function () {
     $breadcrumb = (object) [
         'title' => 'Selamat Datang',
         'list'  => ['Home', 'Welcome']
@@ -27,3 +26,28 @@ Route::get('/dash', function () {
 
     return view('dashboard', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
 });
+
+
+Route::group(['prefix' => 'kategori'], function () {
+    Route::get('/', [KategoriController::class, 'index']);          // menampilkan halaman awal kategori barang
+    Route::post('/list', [KategoriController::class, 'list']);      // menampilkan data kategori barang dalam bentuk json untuk datatables
+    Route::get('/create', [KategoriController::class, 'create']);   // menampilkan halaman form tambah kategori barang
+    Route::post('/', [KategoriController::class, 'store']);         // menyimpan data kategori barang baru
+    Route::get('/{id}', [KategoriController::class, 'show']);       // menampilkan detail kategori barang
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']);  // menampilkan halaman form edit kategori barang
+    Route::put('/{id}', [KategoriController::class, 'update']);     // menyimpan perubahan data kategori barang
+    Route::delete('/{id}', [KategoriController::class, 'destroy']); // menghapus data kategori barang
+});
+
+Route::group(['prefix' => 'arsip'], function () {
+    Route::get('/', [ArsipController::class, 'index']);          // menampilkan halaman awal kategori barang
+    Route::post('/list', [ArsipController::class, 'list']);      // menampilkan data kategori barang dalam bentuk json untuk datatables
+    Route::get('/create', [ArsipController::class, 'create']);   // menampilkan halaman form tambah kategori barang
+    Route::post('/', [ArsipController::class, 'store']);         // menyimpan data kategori barang baru
+    Route::get('/{id}', [ArsipController::class, 'show']);       // menampilkan detail kategori barang
+    Route::get('/{id}/edit', [ArsipController::class, 'edit']);  // menampilkan halaman form edit kategori barang
+    Route::put('/{id}', [ArsipController::class, 'update']);     // menyimpan perubahan data kategori barang
+    Route::delete('/{id}', [ArsipController::class, 'destroy']); // menghapus data kategori barang
+});
+
+Route::get('/about', [AboutController::class, 'index']);           // menampilkan halaman about
